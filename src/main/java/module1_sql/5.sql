97b2c0ae566a42d9f574586ed618f3e191a413e0
GetFire <azhimov93@gmail.com>
2017-02-20 22:16:34 +0200

    Update 5.sql

--- src/main/java/module1_sql/5.sql
+++ src/main/java/module1_sql/5.sql
@@ -1,8 +1,3 @@
--- UPDATE DATA
-INSERT INTO projects (project_name, project_cost, comp_id, cust_id) VALUES
-  ('Тесты для Java Junior', 100, 1, 1),
-  ('Тесты для Objective-C Junior', 150, 1, 1);
-
 -- The main code of task --
 CREATE VIEW  total_cost_by_every_customer AS
   SELECT cust_name,comp_name,SUM(project_cost) as total, COUNT(project_id) as projectNumb
